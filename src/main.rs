9233e15bae1214a091ae687aafec5119609e5ea3
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-07-09 12:14:13 +0200

    connection to server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let stream = TcpStream::connect("127.0.0.1:7878");
+    
+    match stream {
+        Ok(_stream) => println!("[SUCCESS] connected to the server"), 
+        Err(_) => println!("[ERROR] could not connect to the server"),
+    }
 }
